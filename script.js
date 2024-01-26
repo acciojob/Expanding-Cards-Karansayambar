01c1295156f83a148a456ef3476e5526ba879eb1
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-27 00:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const panels = document.querySelectorAll('.panel');
-	const panel2 = document.getElementById("panel-2");
-    panels.forEach(panel => {
-        panel.addEventListener('click', function () {
-            // Remove 'active' class from all panels
-            panels.forEach(otherPanel => {
-                otherPanel.classList.remove('active');
-            });
 
-            // Add 'active' class to the clicked panel
-            this.classList.add('active');
-			panel2.classList.add('active');
+    function handlePanelClick() {
+        // Remove 'active' class from all panels
+        panels.forEach(otherPanel => {
+            otherPanel.classList.remove('active');
         });
+
+        // Add 'active' class to the clicked panel
+        this.classList.add('active');
+    }
+
+    panels.forEach(panel => {
+        panel.addEventListener('load', handlePanelClick);
+        panel.addEventListener('click', handlePanelClick);
     });
 });
