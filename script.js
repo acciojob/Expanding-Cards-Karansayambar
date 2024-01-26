f89d7f5844e7079e693af260f21f6f84c00f45ba
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-26 23:42:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', function () {
+            panels.forEach(otherPanel => {
+                otherPanel.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+});
