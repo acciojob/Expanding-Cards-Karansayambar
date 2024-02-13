d8415e5b5cbd2cb5e8ff45171e04077386ef2041
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-13 10:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const panel = document.querySelector(".panel");
-const text = document.quertSelector(".img-text");
-panel.addEventListener("click", () => {
-	const className ="active"
-	if(panel.classList != className){
-		panel.classList.add(className);
-		text.style.display = "block";
-	}else{
-		panel.classList.remove(className);
-		text.style.display = "none";
-	}
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach((panel) => {
+    const text = panel.querySelector(".img-text");
+
+    panel.addEventListener("click", () => {
+      const className = "active";
+
+      if (!panel.classList.contains(className)) {
+        panel.classList.add(className);
+        text.style.display = "block";
+      } else {
+        panel.classList.remove(className);
+        text.style.display = "none";
+      }
+    });
+  });
+});
