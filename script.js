70a06c37b8392ffddf189e55273d7fbcf4f35955
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-13 11:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,8 +19,11 @@
 // });   not work
 document.addEventListener("DOMContentLoaded", () => {
     const panels = document.querySelectorAll('.panel');
+
+    panels.forEach((panel, index) => {
         panel.addEventListener('click', () => {
             panels.forEach(p => p.classList.remove('active'));
             panel.classList.add('active');
-		});
+        });
+    });
 });
