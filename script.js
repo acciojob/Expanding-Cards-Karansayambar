f9d3b8b7c0af5b18a7c4a5a0b1e44f5388789cf8
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-13 11:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,11 +19,12 @@
 // });   not work
 document.addEventListener("DOMContentLoaded", () => {
     const panels = document.querySelectorAll('.panel');
-
     panels.forEach((panel) => {
         panel.addEventListener('click', () => {
-            panels.forEach(p => p.classList.remove('active'));
+			const text1 = panel.querySelector(".img-text");
+			panels.forEach(p => p.classList.remove('active'));
             panel.classList.add('active');
+			text1.style.display = "flex";
         });
     });
 });
